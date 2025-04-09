fee1bdb883f7b87ee72a47e762077ddafc403193
Thor Tudal Lauridsen <Thor.Lauridsen@ufst.dk>
2025-04-09 09:33:42 +0200

    docs: Add kdocs to CustomerRepo

--- modules/persistence/src/main/kotlin/com/github/thorlauridsen/persistence/CustomerRepo.kt
+++ modules/persistence/src/main/kotlin/com/github/thorlauridsen/persistence/CustomerRepo.kt
@@ -9,8 +9,19 @@ import io.vertx.sqlclient.SqlClient
 import io.vertx.sqlclient.Tuple
 import java.util.UUID
 
+/**
+ * Customer repository implementation.
+ * This class implements the [ICustomerRepo] interface and provides the actual implementation for the methods.
+ * This class is responsible for interacting with the database to perform CRUD operations on the customer table.
+ * @param client [SqlClient] to interact with the database.
+ */
 class CustomerRepo(private val client: SqlClient) : ICustomerRepo {
 
+    /**
+     * Save a customer.
+     * @param customer [CustomerInput] to save.
+     * @return [Customer] retrieved from database.
+     */
     override suspend fun save(customer: CustomerInput): Customer {
         val id = UUID.randomUUID()
 
@@ -21,6 +32,11 @@ class CustomerRepo(private val client: SqlClient) : ICustomerRepo {
         return find(id) ?: error("Failed to insert customer with mail: ${customer.mail}")
     }
 
+    /**
+     * Get a customer given an id.
+     * @param id [UUID] to fetch customer.
+     * @return [Customer] or null if not found.
+     */
     override suspend fun find(id: UUID): Customer? {
         val row = client
             .preparedQuery("SELECT id, mail FROM customer WHERE id = $1")
@@ -31,6 +47,11 @@ class CustomerRepo(private val client: SqlClient) : ICustomerRepo {
         return row?.let { mapRowToCustomer(it) }
     }
 
+    /**
+     * Map a database row to a [Customer] object.
+     * @param row [Row] to map.
+     * @return [Customer] object.
+     */
     private fun mapRowToCustomer(row: Row): Customer {
         return Customer(
             id = row.getUUID("ID"),
