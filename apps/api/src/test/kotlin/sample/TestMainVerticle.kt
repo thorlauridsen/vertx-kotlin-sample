4e0470824bc83bbe82156852a2b8253712f2144f
Thor Tudal Lauridsen <Thor.Lauridsen@ufst.dk>
2025-04-09 09:34:12 +0200

    refac: Reformat code in TestMainVerticle

--- apps/api/src/test/kotlin/sample/TestMainVerticle.kt
+++ apps/api/src/test/kotlin/sample/TestMainVerticle.kt
@@ -11,13 +11,14 @@ import org.junit.jupiter.api.extension.ExtendWith
 @ExtendWith(VertxExtension::class)
 class TestMainVerticle {
 
-  @BeforeEach
-  fun deploy_verticle(vertx: Vertx, testContext: VertxTestContext) {
-    vertx.deployVerticle(MainVerticle()).onComplete(testContext.succeeding<String> { _ -> testContext.completeNow() })
-  }
+    @BeforeEach
+    fun deploy_verticle(vertx: Vertx, testContext: VertxTestContext) {
+        vertx.deployVerticle(MainVerticle())
+            .onComplete(testContext.succeeding<String> { _ -> testContext.completeNow() })
+    }
 
-  @Test
-  fun verticle_deployed(vertx: Vertx, testContext: VertxTestContext) {
-    testContext.completeNow()
-  }
+    @Test
+    fun verticle_deployed(vertx: Vertx, testContext: VertxTestContext) {
+        testContext.completeNow()
+    }
 }
