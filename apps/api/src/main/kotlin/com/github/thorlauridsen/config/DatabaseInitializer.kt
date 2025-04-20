a2ed0b121cce045e6db4370fa4d2006a5d69bf86
Thor Tudal Lauridsen <30320530+REXKrash@users.noreply.github.com>
2025-04-20 20:07:19 +0200

    docs: Update kdocs for DatabaseInitializer

--- apps/api/src/main/kotlin/com/github/thorlauridsen/config/DatabaseInitializer.kt
+++ apps/api/src/main/kotlin/com/github/thorlauridsen/config/DatabaseInitializer.kt
@@ -9,8 +9,22 @@ import liquibase.database.jvm.JdbcConnection
 import liquibase.resource.ClassLoaderResourceAccessor
 import java.sql.DriverManager
 
+/**
+ * This class is responsible for initializing the database.
+ * It creates a connection pool and runs Liquibase migrations.
+ *
+ * @param vertx [Vertx] instance to create the connection pool.
+ */
 class DatabaseInitializer(private val vertx: Vertx) {
 
+    /**
+     * Initialize the database.
+     *
+     * This method creates a connection pool and runs Liquibase migrations.
+     * It returns a [JDBCPool] instance.
+     *
+     * @return [JDBCPool] instance.
+     */
     fun initialize(): JDBCPool {
         val jdbcUrl = "jdbc:h2:~/test"
         val username = "sa"
