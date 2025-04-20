4cd9b8bd01b40be89a83382d8dd19bc08ba19afa
Thor Tudal Lauridsen <30320530+REXKrash@users.noreply.github.com>
2025-04-20 20:02:02 +0200

    refac: Move customer route logic to separate CustomerRouter.kt

--- apps/api/src/main/kotlin/com/github/thorlauridsen/MainVerticle.kt
+++ apps/api/src/main/kotlin/com/github/thorlauridsen/MainVerticle.kt
@@ -1,17 +1,13 @@
 package com.github.thorlauridsen
 
 import com.github.thorlauridsen.config.DatabaseInitializer
-import com.github.thorlauridsen.dto.CustomerInputDto
-import com.github.thorlauridsen.dto.toDto
 import com.github.thorlauridsen.persistence.CustomerRepo
+import com.github.thorlauridsen.route.CustomerRouter.setupCustomerRouter
 import com.github.thorlauridsen.service.CustomerService
-import io.vertx.core.json.JsonObject
 import io.vertx.ext.web.Router
-import io.vertx.ext.web.handler.BodyHandler
 import io.vertx.kotlin.coroutines.CoroutineVerticle
 import io.vertx.kotlin.coroutines.coAwait
 import io.vertx.kotlin.coroutines.coroutineRouter
-import java.util.UUID
 
 class MainVerticle : CoroutineVerticle() {
 
@@ -25,32 +21,7 @@ class MainVerticle : CoroutineVerticle() {
         val customerService = CustomerService(customerRepo)
 
         coroutineRouter {
-            router.get("/customers/:id")
-                .produces("application/json")
-                .coHandler { routingContext ->
-                    val id = routingContext.request().getParam("id")
-                    val uuid = UUID.fromString(id)
-                    val customer = customerService.find(uuid)
-                    val json = JsonObject.mapFrom(customer).encode()
-                    routingContext
-                        .response()
-                        .putHeader("content-type", "application/json")
-                        .end(json)
-                }
-            router.post("/customers")
-                .consumes("application/json")
-                .produces("application/json")
-                .handler(BodyHandler.create())
-                .coHandler { routingContext ->
-                    val body = routingContext.body()
-                    val customer = body.asJsonObject().mapTo(CustomerInputDto::class.java)
-                    val saved = customerService.save(customer.toModel())
-                    val json = JsonObject.mapFrom(saved.toDto()).encode()
-                    routingContext
-                        .response()
-                        .putHeader("content-type", "application/json")
-                        .end(json)
-                }
+            setupCustomerRouter(router, customerService)
         }
         vertx
             .createHttpServer()
---
+++ apps/api/src/main/kotlin/com/github/thorlauridsen/route/CustomerRouter.kt
@@ -0,0 +1,44 @@
+package com.github.thorlauridsen.route
+
+import com.github.thorlauridsen.dto.CustomerInputDto
+import com.github.thorlauridsen.dto.toDto
+import com.github.thorlauridsen.service.CustomerService
+import io.vertx.core.json.JsonObject
+import io.vertx.ext.web.Router
+import io.vertx.ext.web.handler.BodyHandler
+import io.vertx.kotlin.coroutines.CoroutineRouterSupport
+import java.util.UUID
+
+object CustomerRouter {
+
+    fun CoroutineRouterSupport.setupCustomerRouter(router: Router, customerService: CustomerService) {
+
+        router.get("/customers/:id")
+            .produces("application/json")
+            .coHandler { routingContext ->
+                val id = routingContext.request().getParam("id")
+                val uuid = UUID.fromString(id)
+                val customer = customerService.find(uuid)
+                val json = JsonObject.mapFrom(customer).encode()
+                routingContext
+                    .response()
+                    .putHeader("content-type", "application/json")
+                    .end(json)
+            }
+
+        router.post("/customers")
+            .consumes("application/json")
+            .produces("application/json")
+            .handler(BodyHandler.create())
+            .coHandler { routingContext ->
+                val body = routingContext.body()
+                val customer = body.asJsonObject().mapTo(CustomerInputDto::class.java)
+                val saved = customerService.save(customer.toModel())
+                val json = JsonObject.mapFrom(saved.toDto()).encode()
+                routingContext
+                    .response()
+                    .putHeader("content-type", "application/json")
+                    .end(json)
+            }
+    }
+}
