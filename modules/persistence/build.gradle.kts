e7a7f8f6b1677bc58369f2140221b43a12f32d7a
Thor Tudal Lauridsen <30320530+REXKrash@users.noreply.github.com>
2025-04-22 18:25:23 +0200

    build: Reformat build.gradle.kts in persistence module

--- modules/persistence/build.gradle.kts
+++ modules/persistence/build.gradle.kts
@@ -2,11 +2,10 @@ dependencies {
     // Persistence subproject needs to know about the model subproject
     implementation(projects.model)
 
-    implementation(platform(local.vertx.stack.depchain))
-
-    implementation(local.vertx.pg.client)
+    // Vert.x dependencies
     implementation(local.vertx.jdbc.client)
-
-    implementation(local.vertx.lang.kotlin.coroutines)
     implementation(local.vertx.lang.kotlin)
+    implementation(local.vertx.lang.kotlin.coroutines)
+    implementation(local.vertx.pg.client)
+    implementation(platform(local.vertx.stack.depchain))
 }
