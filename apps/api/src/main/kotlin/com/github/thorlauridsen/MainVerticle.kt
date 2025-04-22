f6805adb3f024eaa97718810ee565ac14377361f
Thor Tudal Lauridsen <30320530+REXKrash@users.noreply.github.com>
2025-04-22 18:13:13 +0200

    refac: Move Swagger/OpenAPI setup logic to SwaggerRouter

--- apps/api/src/main/kotlin/com/github/thorlauridsen/MainVerticle.kt
+++ apps/api/src/main/kotlin/com/github/thorlauridsen/MainVerticle.kt
@@ -3,10 +3,9 @@ package com.github.thorlauridsen
 import com.github.thorlauridsen.config.DatabaseInitializer
 import com.github.thorlauridsen.persistence.CustomerRepo
 import com.github.thorlauridsen.route.CustomerRouter.setupCustomerRouter
+import com.github.thorlauridsen.route.SwaggerRouter.setupStaticRouter
 import com.github.thorlauridsen.service.CustomerService
 import io.vertx.ext.web.Router
-import io.vertx.ext.web.handler.StaticHandler
-import io.vertx.ext.web.openapi.RouterBuilder
 import io.vertx.kotlin.coroutines.CoroutineVerticle
 import io.vertx.kotlin.coroutines.coAwait
 import io.vertx.kotlin.coroutines.coroutineRouter
@@ -37,21 +36,7 @@ class MainVerticle : CoroutineVerticle() {
 
         coroutineRouter {
             setupCustomerRouter(router, customerService)
-        }
-
-        router.route("/static/*").handler(StaticHandler.create("webroot"))
-        router.get("/swagger").handler { ctx ->
-            ctx.response()
-                .sendFile("webroot/swagger-ui.html")
-        }
-        router.get("/openapi.yaml").handler { ctx ->
-            vertx.fileSystem().readFile("src/main/resources/openapi.yaml").onSuccess { buffer ->
-                ctx.response()
-                    .putHeader("content-type", "text/yaml")
-                    .end(buffer)
-            }.onFailure { err ->
-                ctx.fail(err)
-            }
+            setupStaticRouter(router, vertx)
         }
         vertx
             .createHttpServer()
---
+++ apps/api/src/main/kotlin/com/github/thorlauridsen/route/SwaggerRouter.kt
@@ -0,0 +1,34 @@
+package com.github.thorlauridsen.route
+
+import io.vertx.core.Vertx
+import io.vertx.ext.web.Router
+import io.vertx.ext.web.handler.StaticHandler
+
+/**
+ * Static router for defining the routes related to Swagger/OpenAPI documentation.
+ */
+object SwaggerRouter {
+
+    /**
+     * Sets up the HTTP endpoints for static files for Swagger/OpenAPI documentation.
+     *
+     * @param router The router to set up.
+     * @param vertx The Vertx instance to access the file system.
+     */
+    fun setupStaticRouter(router: Router, vertx: Vertx) {
+        router.route("/static/*").handler(StaticHandler.create("webroot"))
+        router.get("/swagger").handler { ctx ->
+            ctx.response().sendFile("webroot/swagger-ui.html")
+        }
+        router.get("/openapi.yaml").handler { ctx ->
+            vertx.fileSystem().readFile("src/main/resources/openapi.yaml").onSuccess { buffer ->
+                ctx.response()
+                    .putHeader("content-type", "text/yaml")
+                    .end(buffer)
+
+            }.onFailure { err ->
+                ctx.fail(err)
+            }
+        }
+    }
+}
