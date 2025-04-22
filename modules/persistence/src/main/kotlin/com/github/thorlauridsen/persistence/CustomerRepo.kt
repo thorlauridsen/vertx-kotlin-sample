64abde2f55bd21145baa3749c6d4bf2aa16885d3
Thor Tudal Lauridsen <30320530+REXKrash@users.noreply.github.com>
2025-04-22 18:03:13 +0200

    feat: Add docker-compose.yml

--- modules/persistence/src/main/kotlin/com/github/thorlauridsen/persistence/CustomerRepo.kt
+++ modules/persistence/src/main/kotlin/com/github/thorlauridsen/persistence/CustomerRepo.kt
@@ -25,7 +25,7 @@ class CustomerRepo(private val client: SqlClient) : ICustomerRepo {
     override suspend fun save(customer: CustomerInput): Customer {
         val id = UUID.randomUUID()
 
-        client.preparedQuery("INSERT INTO customer(id, mail) VALUES ($1, $2)")
+        client.preparedQuery("INSERT INTO customer(id, mail) VALUES (?, ?)")
             .execute(Tuple.of(id, customer.mail))
             .coAwait()
 
@@ -38,8 +38,7 @@ class CustomerRepo(private val client: SqlClient) : ICustomerRepo {
      * @return [Customer] or null if not found.
      */
     override suspend fun find(id: UUID): Customer? {
-        val row = client
-            .preparedQuery("SELECT id, mail FROM customer WHERE id = $1")
+        val row = client.preparedQuery("SELECT id, mail FROM customer WHERE id = ?")
             .execute(Tuple.of(id))
             .coAwait()
             .firstOrNull()
@@ -49,13 +48,46 @@ class CustomerRepo(private val client: SqlClient) : ICustomerRepo {
 
     /**
      * Map a database row to a [Customer] object.
+     *
+     * Please note that column names in H2 are case-insensitive
+     * while column names in PostgreSQL are case-sensitive.
+     * This function handles both cases when mapping the row to a [Customer] object.
+     *
      * @param row [Row] to map.
      * @return [Customer] object.
      */
     private fun mapRowToCustomer(row: Row): Customer {
         return Customer(
-            id = row.getUUID("ID"),
-            mail = row.getString("MAIL")
+            id = row.getRowUUID("id"),
+            mail = row.getRowString("mail")
         )
+    }
+
+    /**
+     * Extension function to get a UUID value from the row with a case-insensitive column name.
+     *
+     * @param columnName Name of the column to get the value from.
+     * @return [UUID] value.
+     */
+    private fun Row.getRowUUID(columnName: String): UUID {
+        return try {
+            getUUID(columnName.lowercase())
+        } catch (_: Exception) {
+            getUUID(columnName.uppercase())
+        }
+    }
+
+    /**
+     * Extension function to get a string value from the row with a case-insensitive column name.
+     *
+     * @param columnName Name of the column to get the value from.
+     * @return [String] value.
+     */
+    private fun Row.getRowString(columnName: String): String {
+        return try {
+            getString(columnName.lowercase())
+        } catch (_: Exception) {
+            getString(columnName.uppercase())
+        }
     }
 }
