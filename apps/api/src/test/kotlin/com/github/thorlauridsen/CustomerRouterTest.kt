d31cced00b0d1d0cf736ca5fc8e3697b89723589
Thor Tudal Lauridsen <Thor.Lauridsen@ufst.dk>
2025-04-23 10:32:03 +0200

    refac: Improve CustomerRouterTest
    Update CustomerRouterTest so the test functions does not take both the vertx instance and test context as a parameter. Instead the vertx instance and test context are available globally for this class.

--- apps/api/src/test/kotlin/com/github/thorlauridsen/CustomerRouterTest.kt
+++ apps/api/src/test/kotlin/com/github/thorlauridsen/CustomerRouterTest.kt
@@ -9,18 +9,29 @@ import io.vertx.ext.web.client.WebClientOptions
 import io.vertx.junit5.VertxExtension
 import io.vertx.junit5.VertxTestContext
 import java.util.UUID
+import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.extension.ExtendWith
 
 @ExtendWith(VertxExtension::class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CustomerRouterTest {
 
-    private val clientOptions = WebClientOptions().setDefaultHost("localhost").setDefaultPort(8080)
+    private lateinit var vertx: Vertx
+    private lateinit var client: WebClient
 
-    @BeforeEach
-    fun deployVerticle(vertx: Vertx, testContext: VertxTestContext) {
+    private val clientOptions = WebClientOptions()
+        .setDefaultHost("localhost")
+        .setDefaultPort(8080)
+
+    @BeforeAll
+    fun setup(vertx: Vertx, testContext: VertxTestContext) {
+        this.vertx = vertx
+        client = WebClient.create(vertx, clientOptions)
+
         vertx.deployVerticle(MainVerticle()) { ar ->
             if (ar.succeeded()) {
                 testContext.completeNow()
@@ -30,49 +41,47 @@ class CustomerRouterTest {
         }
     }
 
+    @AfterAll
+    fun tearDown() {
+        vertx.close()
+    }
+
     @Test
-    fun `get customer - random id - returns not found`(vertx: Vertx, testContext: VertxTestContext) {
-        val client = WebClient.create(vertx, clientOptions)
+    fun `get customer - random id - returns not found`() {
         val id = UUID.randomUUID()
 
         client.get("/customers/$id").send { ar ->
             if (ar.succeeded()) {
                 val response = ar.result()
-                testContext.verify {
-                    assertEquals(HttpResponseStatus.NOT_FOUND.code(), response.statusCode())
-                    testContext.completeNow()
-                }
+                assertEquals(HttpResponseStatus.NOT_FOUND.code(), response.statusCode())
+
             } else {
-                testContext.failNow(ar.cause())
+                throw ar.cause()
             }
         }
     }
 
     @Test
-    fun `post customer - get customer - success`(vertx: Vertx, testContext: VertxTestContext) {
-        val client = WebClient.create(vertx, clientOptions)
+    fun `post customer - get customer - success`() {
         val customer = CustomerInputDto(mail = "test@example.com")
         val customerJson = JsonObject.mapFrom(customer)
         var customerId: String? = null
 
         client.post("/customers")
             .sendJsonObject(customerJson)
-            .compose { postRes ->
-                testContext.verify {
-                    assertEquals(HttpResponseStatus.OK.code(), postRes.statusCode())
-                    assertEquals(customer.mail, postRes.bodyAsJsonObject().getString("mail"))
-                }
-                customerId = postRes.bodyAsJsonObject().getString("id")
+            .compose { postResponse ->
+                assertEquals(HttpResponseStatus.OK.code(), postResponse.statusCode())
+                assertEquals(customer.mail, postResponse.bodyAsJsonObject().getString("mail"))
+
+                customerId = postResponse.bodyAsJsonObject().getString("id")
                 client.get("/customers/$customerId").send()
             }
-            .onSuccess { getRes ->
-                testContext.verify {
-                    assertEquals(HttpResponseStatus.OK.code(), getRes.statusCode())
-                    val json = getRes.bodyAsJsonObject()
-                    assertEquals(customerId, json.getString("id"))
-                    assertEquals(customer.mail, json.getString("mail"))
-                }
-                testContext.completeNow()
+            .onSuccess { getResponse ->
+                assertEquals(HttpResponseStatus.OK.code(), getResponse.statusCode())
+                val json = getResponse.bodyAsJsonObject()
+
+                assertEquals(customerId, json.getString("id"))
+                assertEquals(customer.mail, json.getString("mail"))
             }
     }
 }
